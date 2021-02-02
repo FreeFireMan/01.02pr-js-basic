b6b390a7ccf2c1cdf9c480cb152c8cbf547ac8ad
NataliiaRybalka <natalirybalka21@gmail.com>
2021-02-02 10:44:57 +0200

    #6

---
+++ main.js
@@ -0,0 +1,93 @@
+// #1
+let hello = 'Hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let a = 1;
+let b = 10;
+let c = -999;
+let d = 123;
+let e = 3.14;
+let f = 2.7;
+let g = 16;
+let boolTrue = true;
+let boolFalse = false;
+
+console.log(hello);
+console.log(owu + '.' + com + '.' + ua);
+console.log(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g);
+console.log(boolTrue + ' ' + boolFalse);
+
+/* alert(hello);
+alert(owu + '.' + com + '.' + ua);
+alert(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g);
+alert(boolTrue + ' ' + boolFalse); */
+
+document.write(hello);
+document.write('<hr>');
+document.write(owu + '.' + com + '.' + ua);
+document.write('<hr>');
+document.write(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g);
+document.write('<hr>');
+document.write(boolTrue + ' ' + boolFalse);
+document.write('<hr>');
+document.write('<hr>');
+
+// #2
+hello = 'Goodbye';
+owu = 'OWU';
+com = 'COM';
+ua = 'UA';
+a = 3;
+b = 55;
+c = -2612;
+d = 362;
+e = 6.191;
+f = 6.7;
+g = 88;
+boolTrue = 'true';
+boolFalse = 'false';
+
+console.log(hello);
+console.log(owu + '.' + com + '.' + ua);
+console.log(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g);
+console.log(boolTrue + ' ' + boolFalse);
+
+/* alert(hello);
+alert(owu + '.' + com + '.' + ua);
+alert(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g);
+alert(boolTrue + ' ' + boolFalse); */
+
+document.write(hello);
+document.write('<hr>');
+document.write(owu + '.' + com + '.' + ua);
+document.write('<hr>');
+document.write(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g);
+document.write('<hr>');
+document.write(boolTrue + ' ' + boolFalse);
+document.write('<hr>');
+document.write('<hr>');
+
+// #3
+/* let userName = prompt('What is your name?');
+let userPatronymic = prompt('What is your patronymic?');
+let userSurname = prompt('What is your surname?');
+alert('Hello, ' + userName + ' ' + userPatronymic + ' ' + userSurname); 
+
+// #4
+let fullName = userName + ' ' + userPatronymic + ' ' + userSurname; */
+
+// #5
+/* let first = prompt('Enter the number:');
+let second = prompt('Enter the number:');
+let third = prompt('Enter the number:');
+console.log(parseInt(first));
+console.log(parseInt(second));
+console.log(parseInt(third)); */
+
+// #6
+/* let first = prompt('Enter the number:');
+let second = prompt('Enter the number:');
+let third = prompt('Enter the number:');
+let sum = parseInt(first) + parseInt(second) + parseInt(third);
+console.log(sum); */
