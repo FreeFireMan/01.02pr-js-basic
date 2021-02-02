e5078a53a8af2b183649e5cd83ff534e82d7be01
NataliiaRybalka <natalirybalka21@gmail.com>
2021-02-02 12:07:17 +0200

    #11

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // #1
-let hello = 'Hello';
+/* let hello = 'Hello';
 let owu = 'owu';
 let com = 'com';
 let ua = 'ua';
@@ -18,10 +18,10 @@ console.log(owu + '.' + com + '.' + ua);
 console.log(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g);
 console.log(boolTrue + ' ' + boolFalse);
 
-/* alert(hello);
+alert(hello);
 alert(owu + '.' + com + '.' + ua);
 alert(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g);
-alert(boolTrue + ' ' + boolFalse); */
+alert(boolTrue + ' ' + boolFalse);
 
 document.write(hello);
 document.write('<hr>');
@@ -31,10 +31,10 @@ document.write(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g);
 document.write('<hr>');
 document.write(boolTrue + ' ' + boolFalse);
 document.write('<hr>');
-document.write('<hr>');
+document.write('<hr>'); */
 
 // #2
-hello = 'Goodbye';
+/* hello = 'Goodbye';
 owu = 'OWU';
 com = 'COM';
 ua = 'UA';
@@ -53,10 +53,10 @@ console.log(owu + '.' + com + '.' + ua);
 console.log(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g);
 console.log(boolTrue + ' ' + boolFalse);
 
-/* alert(hello);
+alert(hello);
 alert(owu + '.' + com + '.' + ua);
 alert(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g);
-alert(boolTrue + ' ' + boolFalse); */
+alert(boolTrue + ' ' + boolFalse);
 
 document.write(hello);
 document.write('<hr>');
@@ -66,7 +66,7 @@ document.write(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g);
 document.write('<hr>');
 document.write(boolTrue + ' ' + boolFalse);
 document.write('<hr>');
-document.write('<hr>');
+document.write('<hr>'); */
 
 // #3
 /* let userName = prompt('What is your name?');
@@ -91,3 +91,46 @@ let second = prompt('Enter the number:');
 let third = prompt('Enter the number:');
 let sum = parseInt(first) + parseInt(second) + parseInt(third);
 console.log(sum); */
+
+// #7
+/* let first = prompt('Enter the fractional number:');
+let second = prompt('Enter the fractional number:');
+let third = prompt('Enter the fractional number:');
+let result = parseFloat(first) + parseFloat(second) + parseFloat(third);
+console.log(result); */
+
+// #8
+/* let first = prompt('Enter the fractional number:');
+let second = prompt('Enter the fractional number:');
+let third = prompt('Enter the fractional number:');
+let result = Math.round(first) + Math.round(second) + Math.round(third);
+console.log(result); */
+
+// #9
+/* let first = prompt('Enter the number:');
+let second = prompt('Enter the number:');
+let result = Math.pow(first, second);
+console.log(result); */
+
+// #10
+/* let a = 100; 
+let b = '100'; 
+let c = true;
+
+console.log(a, typeof(a));
+console.log(b, typeof(b));
+console.log(c, typeof(c)); */
+
+// #11
+console.log(5 < 6);
+console.log(5 === 6);
+console.log(5 >= 6);
+console.log(5 > 6);
+console.log(10 === 10);
+console.log(10 >= 10);
+console.log(10 > 10);
+console.log(10 !== 10);
+console.log(10 < 10);
+console.log(123 === '123');
+console.log(123 == '123');
+
